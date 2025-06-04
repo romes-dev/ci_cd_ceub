6ff71587ff7cad331fc6cc861acdbe8ea94578c0
Romes <romes.heriberto@gmail.com>
2025-06-04 19:58:24 -0300

    Ajustes com CICD

--- olamundo-django/olamundo/core/tests.py
+++ olamundo-django/olamundo/core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
+class HelloWorldTestCase(TestCase):
+    def test_hello_world(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello, World!")
+
 # Create your tests here.
